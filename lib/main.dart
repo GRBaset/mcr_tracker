6e2823ddb9b2637d85db03410532e9de06320e10
GRBaset <info@grbaset.com>
2026-03-28 16:27:09 +0100

    Upgrade Flutter to 3.41.6

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:collection';
 
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'l10n/app_localizations.dart';
 import 'dart:convert';
 
 import 'package:uuid/uuid.dart';
